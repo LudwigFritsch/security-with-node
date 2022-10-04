b349ca48693e7c79a0af22eef751ce890bd4274f
Ludwig Fritsch <ludwig-fritsch@outlook.de>
2022-10-04 15:02:54 +0200

    Add placeholder endpoint for social sign in with google

--- server.js
+++ server.js
@@ -8,14 +8,30 @@ const PORT = process.env.PORT || 8000;
 
 const app = express();
 
+function checkLoggedIn(req, res, next) {
+  const isLoggedIn = true; // TODO
+  if (!isLoggedIn) {
+    return res.status(401).json({ error: "You must log in" });
+  }
+  next();
+}
+
 app.use(helmet());
 
-app.get("/secret", (req, res) => {
-  return res.send("Personal secret");
+app.use(express.static(__dirname + "/public"));
+
+app.get("/auth/google", (req, res) => {});
+
+app.get("/auth/google/callback", (req, res) => {});
+
+app.get("/auth/logout", (req, res) => {});
+
+app.get("/secret", checkLoggedIn, (req, res) => {
+  return res.sendFile(path.join(__dirname, "public", "secret.html"));
 });
 
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "index.html"));
+app.get("/*", (req, res) => {
+  return res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
 const server = https.createServer(
