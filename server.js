4e2870c1cd22eb373378f6bb368781333f7bf2e5
Ludwig Fritsch <ludwig-fritsch@outlook.de>
2022-10-10 21:31:47 +0200

    Implement google callback handler

--- server.js
+++ server.js
@@ -9,15 +9,10 @@ require("dotenv").config();
 
 const PORT = process.env.PORT || 8000;
 
-const config = {
-  CLIENT_ID: process.env.CLIENT_ID,
-  CLIENT_SECRET: process.env.CLIENT_SECRET,
-};
-
 const AUTH_OPTIONS = {
   callbackURL: "/auth/google/callback",
-  clientID: config.CLIENT_ID,
-  clientSecret: config.CLIENT_SECRET,
+  clientID: process.env.CLIENT_ID,
+  clientSecret: process.env.CLIENT_SECRET,
 };
 
 function verifyCallback(accessToken, refreshToken, profile, done) {
@@ -35,14 +30,28 @@ app.use(passport.initialize());
 
 app.use(express.static(__dirname + "/public"));
 
-app.get("/auth/google", (req, res) => {});
+app.get("/auth/google", passport.authenticate("google", { scope: ["email"] }));
 
-app.get("/auth/google/callback", (req, res) => {});
+app.get(
+  "/auth/google/callback",
+  passport.authenticate("google", {
+    failureRedirect: "/failure",
+    successRedirect: "/",
+    session: false,
+  }),
+  (req, res) => {
+    console.log("Google called us back");
+  }
+);
 
 app.get("/auth/logout", (req, res) => {});
 
 app.get("/secret", checkLoggedIn, (req, res) => {
   return res.sendFile(path.join(__dirname, "public", "secret.html"));
+});
+
+app.get("/failure", (req, res) => {
+  return res.send("Failed to login");
 });
 
 app.get("/*", (req, res) => {
